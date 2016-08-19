8d461614fb60fa17dee6156d8b982f15e0afc678
SteeleVR <derrick.l.steele@gmail.com>
2016-08-19 00:06:19 -0500

    Create ht_sql_queries_002.sql

---
+++ ht_sql_queries_002.sql
@@ -0,0 +1,14 @@
+CREATE TABLE hotels ( hotelid char(10) not null, hotelname char(30) not null, primary key(hotelid) );
+
+load data infile '/home/admin/hotels.csv' replace into table hotels fields terminated by ',' lines terminated by '\n';
+
+DELETE FROM hotels
+ORDER BY hotelid DESC LIMIT 1;
+
+SELECT COUNT(bookings.hotelid) as total_num_bookings, hotels.hotelname
+FROM bookings
+LEFT JOIN hotels
+ON bookings.hotelid=hotels.hotelid
+WHERE bookings.iso_checkin between '2015-02-03' and '2015-12-24'
+GROUP BY bookings.hotelid
+ORDER BY total_num_bookings DESC;
